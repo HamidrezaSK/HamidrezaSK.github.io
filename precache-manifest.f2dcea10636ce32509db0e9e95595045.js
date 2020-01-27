b949084e321bbf4e4fb33475ea8bb561d8672ac4
Hamidreza Sanaee <shrsanaee@gmail.com>
2020-01-27 12:47:57 +0330

    Deploy

---
+++ precache-manifest.f2dcea10636ce32509db0e9e95595045.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5fcf87d82186850bf99c1c1aaeccf3b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bd47c48cb5aaa0dec50",
+    "url": "/static/css/2.d6181cb7.chunk.css"
+  },
+  {
+    "revision": "05e7dbd4b3486ac40b3e",
+    "url": "/static/css/main.ee04dd2f.chunk.css"
+  },
+  {
+    "revision": "3bd47c48cb5aaa0dec50",
+    "url": "/static/js/2.952ce79d.chunk.js"
+  },
+  {
+    "revision": "05e7dbd4b3486ac40b3e",
+    "url": "/static/js/main.ab4a68ff.chunk.js"
+  },
+  {
+    "revision": "6205a8d70b4e3f7509e1",
+    "url": "/static/js/runtime-main.39007d83.js"
+  },
+  {
+    "revision": "d67a34fe32c4d8c9a5d05504a32c7b5b",
+    "url": "/static/media/3-4-3.d67a34fe.png"
+  },
+  {
+    "revision": "7fedcb41d881d2ba506e26b21bebd61b",
+    "url": "/static/media/3-5-2.7fedcb41.png"
+  },
+  {
+    "revision": "3c0ec32428381d0308066f17e686e230",
+    "url": "/static/media/4-3-3.3c0ec324.png"
+  },
+  {
+    "revision": "a1367e76035449e12c6e4708665ddd11",
+    "url": "/static/media/4-4-2.a1367e76.png"
+  },
+  {
+    "revision": "05bebd2a6a4fbcb2162248f319fc9956",
+    "url": "/static/media/4-5-1.05bebd2a.png"
+  },
+  {
+    "revision": "7fe1a205c4f6e145a8b61e39baa24fa7",
+    "url": "/static/media/5-2-3.7fe1a205.png"
+  },
+  {
+    "revision": "d6b752b8db7ab355db9a4b8b5a601a03",
+    "url": "/static/media/5-3-2.d6b752b8.png"
+  },
+  {
+    "revision": "dcc4d763683b79143ecff308aa407ea0",
+    "url": "/static/media/5-4-1.dcc4d763.png"
+  },
+  {
+    "revision": "d02d119ee231eca650084e8d022ac48f",
+    "url": "/static/media/Daniel Ceballos Fernández.d02d119e.jpg"
+  },
+  {
+    "revision": "14c8f4f32dcd41615347f8c4e70e8184",
+    "url": "/static/media/David Luiz Moreira Marinho.14c8f4f3.jpg"
+  },
+  {
+    "revision": "c20900c85d5748649eb447fb27800c61",
+    "url": "/static/media/GK.c20900c8.svg"
+  },
+  {
+    "revision": "583f566b9a2810bf77a6bfdfa11a0b0a",
+    "url": "/static/media/bench.583f566b.svg"
+  },
+  {
+    "revision": "314d90241023d4899c7bbba541d35a2a",
+    "url": "/static/media/football-player.314d9024.svg"
+  },
+  {
+    "revision": "a9944341bb1140b2c70c15f4059f3c19",
+    "url": "/static/media/p98745.a9944341.png"
+  },
+  {
+    "revision": "318c0a2072338ec8d7416c9633f787cf",
+    "url": "/static/media/pitch.318c0a20.318c0a20.svg"
+  },
+  {
+    "revision": "7eb15f60126ff670b2d5547d729c086b",
+    "url": "/static/media/player-comp-3-1x.7eb15f60.7eb15f60.png"
+  },
+  {
+    "revision": "7eb15f60126ff670b2d5547d729c086b",
+    "url": "/static/media/src__assets_player-comp-3-1x.7eb15f60.7eb15f60.png"
+  },
+  {
+    "revision": "5e8128ceb2730172b60928e9ef228259",
+    "url": "/static/media/transfer1.5e8128ce.svg"
+  },
+  {
+    "revision": "9b64087e520bb85f2362daff0a7d236a",
+    "url": "/static/media/transfer2.9b64087e.svg"
+  }
+]);
